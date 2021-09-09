de286fa2005656a2340ca247f46849b9e63cb834
j-linda <linda.jrg20@gmail.com>
2021-09-08 21:29:11 -0500

    Entrega Parcial Front3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TodoLoPrincipal from "./components/TodoLoPrincipal";
+import React from "react";
+
+function App() {
+  return (
+    <div className="layout">
+      <TodoLoPrincipal/>
+    </div>
+  );
+}
+
+export default App;
+
